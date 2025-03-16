1e3124835c25c4c8389361544adf9dbb4ad7bb6a
sarim-alias <sarimsitenative@gmail.com>
2025-03-16 23:59:27 +0500

    feat/changes login

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://143.110.236.119'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://143.110.236.119:3000'],
     credentials: true
 }));
 
