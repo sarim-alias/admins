57c80a9a1cc8265107743a5369f97778142b7280
sarim-alias <sarimsitenative@gmail.com>
2025-03-20 02:50:56 +0500

    feat/user

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,9 +8,9 @@ function App() {
 
   return (
     <>
-      <div className='min-h-screen'>
-        <h1 className='text-xl font-bold text-red-300'>Sarim</h1>
-      </div>    
+      <div>
+        <h1 className='text-2xl bg-orange-400'>Sarim</h1>
+      </div>
     </>
   )
 }
